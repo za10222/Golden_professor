910689a80f5ad78a4686f4c251c0fc457c4ed4ab
za10222 <za10222@163.com>
2019-10-20 13:42:31 +0800

    ui

---
+++ Assets/Scripe/Button_quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button_quit : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        
+        #else
+                Application.Quit();
+        #endif
+
+    }
+}
+    
